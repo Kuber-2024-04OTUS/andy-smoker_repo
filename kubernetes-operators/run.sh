bca17fb7be942d2282d13a6ce2782cd9f1a599b9
Andrey Balan <abalan@hr-link.ru>
2024-07-28 20:45:11 +0300

    .

--- kubernetes-operators/run.sh
+++ kubernetes-operators/run.sh
@@ -5,9 +5,9 @@ cd kubernetes-operators || echo ''
 
 CLUSTER_NAME=minikube
 
-kubectl apply --cluster ${CLUSTER_NAME} -f namespace.yaml
+kubectl apply --cluster ${CLUSTER_NAME} -f crd.yaml -f namespace.yaml
 kubectl config set-context --current --namespace=homework
-kubectl apply --cluster ${CLUSTER_NAME}  -f crd.yaml -f deployment.yaml -f custom.yaml
+kubectl apply --cluster ${CLUSTER_NAME} -f deployment.yaml -f custom.yaml
 
 sleep 5
 kubectl get mysql
